5a0c4da2fa60c2cb3250cd33a43e6169b9b3fc8e
Maxime Devalland <mdevalland@opentable.com>
2018-03-09 14:15:23 +0000

    Ubuntu 16.04+ compatibility

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,15 +3,30 @@ class exhibitor::service(
   $config_dir = $exhibitor::config_dir
 ) {
 
-  file { '/etc/init.d/exhibitor':
-    ensure  => present,
-    content => template('exhibitor/init.d.erb'),
-    mode    => '0755',
-    require => Exec['download-exhibitor']
+  if $::lsbdistrelease in ['16.04'] {
+    file { '/lib/systemd/system/exhibitor.service':
+      mode    => '0644',
+      owner   => 'root',
+      group   => 'root',
+      content => template('exhibitor/exhibitor.systemd.erb'),
+    }~>
+    exec { 'exhibitor-systemd-reload':
+      command     => 'systemctl daemon-reload',
+      path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+      refreshonly => true,
+    }
   }
+  else {
+    file { '/etc/init.d/exhibitor':
+      ensure  => present,
+      content => template('exhibitor/init.d.erb'),
+      mode    => '0755',
+      require => Exec['download-exhibitor']
+    }
 
-  service { 'exhibitor':
-    ensure => 'running',
-    require => File['/etc/init.d/exhibitor']
+    service { 'exhibitor':
+      ensure => 'running',
+      require => File['/etc/init.d/exhibitor']
+    }
   }
 }
